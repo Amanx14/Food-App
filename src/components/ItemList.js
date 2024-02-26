ad8495c917a3a306757a431cfb1b671b5e11e257
Amanx14 <amannkumar14@gmail.com>
2024-02-27 00:46:59 +0530

    Fixed items not rendering

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -6,17 +6,21 @@ const ItemList = ({ items }) => {
     return (
         <div>
             {items ? items.map((item) => (
-                <div key={item.card.info.id} className="p-2 m-2 border-none rounded-md border-gray-400 text-left shadow-lg bg-slate-100">
-                    <div className="flex justify-between mb-2 py-2">
-                        <div>
-                            <div className="mb-3">
-                                <span className="font-bold">{item.card.info.name}</span>
-                                <span className="px-2 font-bold">- ₹{item.card.info.price / 100 || item.card.info.defaultPrice / 100} </span>
-                            </div>
-                            <p className="text-xs">{item.card.info.description}</p>
+                <div key={item.card.info.id} className="p-2 m-2 border-none rounded-md border-gray-400 text-left shadow-lg bg-slate-100 flex justify-between items-center">
+
+                    <div className="w-10/12">
+                        <div className="mb-3">
+                            <span className="font-bold">{item.card.info.name}</span>
+                            <span className="px-2 font-bold">- ₹{item.card.info.price / 100 || item.card.info.defaultPrice / 100} </span>
                         </div>
-                        <img className="w-[auto] h-[75px] rounded-md mx-2" src={CDN_URL + item.card.info.imageId} alt="" />
+                        <p className="text-xs">{item.card.info.description}</p>
                     </div>
+
+                    <div className="w-2/12 mr-3 flex flex-col" >
+                        <img className="rounded-lg mx-1" src={CDN_URL + item.card.info.imageId} alt="" />
+                        <button className="px-2 rounded-md bg-slate-700 text-white shadow-lg">Add +</button>
+                    </div>
+
 
                 </div>
             )) :
--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,19 +1,24 @@
+import { useState } from "react";
 import ItemList from "./ItemList";
 
 const RestaurantCategory = ({ data }) => {
     // console.log("----------", data.itemCards, data.title);
-    const {title , itemCards} = data;
+    const { title, itemCards } = data;
+
+    const [showItems, setShowItems] = useState(false);
+
+    const handleClick = () => {
+        setShowItems(!showItems);
+    }
 
     return (
         <div>
             <div className="w-6/12 bg-gray-50 shadow-lg mx-auto my-4 p-4 rounded-lg">
-                <div className="flex justify-between">
-                    <span className="font-bold text-md">{title}</span>
-                    <span> <i className="fa-solid fa-angle-down"></i> </span>
+                <div className="flex justify-between cursor-pointer" onClick={handleClick}>
+                    <span className="font-bold text-xl">{title}</span>
+                    <span><i className="fa-solid fa-angle-down"></i></span>
                 </div>
-
-                <ItemList items={itemCards} />
-
+                {showItems && <ItemList items={itemCards} />}
             </div>
         </div>
     );
