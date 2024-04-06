0bcc87cb682e19210df6d975939f42c842ae6029
Protox14 <aman.kumar@ens.enterprises>
2024-04-06 12:12:59 +0530

    Testing our app

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,17 @@
 const Contact = ()=> {
     return (
         <div>
-            <h1>Contact Us</h1>
-            <h2>I don't have a phone number</h2>
+            <h1 className="font-bold text-3xl p-2 m-1">Contact Us</h1>
+            {/* <h2 className="font-bold text-xl px-3">I don't have a phone number</h2> */}
+
+            <form action="">
+                <label htmlFor="name" className="mx-2">Name : </label>
+                <input type="text" id ="name" className="border border-black p-2 m-2 rounded-sm focus:outline-none focus:border-green-300" placeholder="Enter your name"/>
+
+                <label htmlFor="message" className="mx-2">Message : </label>
+                <input type="text" id="message" className="border border-black p-2 m-2 rounded-sm focus:outline-none focus:border-green-300" placeholder="Enter your message"/>
+                <button className="px-4 py-1 bg-black text-white rounded-md">Submit</button>
+            </form>
         </div>
     );
 }
---
+++ src/components/__test__/contact.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import Contact from "../Contact";
+import "@testing-library/jest-dom";
+
+test("Should load contact us component", () => {
+    render(<Contact />);
+
+    const heading = screen.getByRole("heading");
+
+    expect(heading).toBeInTheDocument();
+});
+
+test("Should load button on the component", () => {
+    render(<Contact />);
+
+    // const button = screen.getByRole("button");
+    const button = screen.getByText("Submit");
+
+    expect(button).toBeInTheDocument(); // Assertion
+});
